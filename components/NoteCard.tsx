26079f80545e6b86da027194e908005abf95018a
“JosephKom” <jokmr17@gmail.com>
2025-08-12 15:37:18 -0700

    -fixed sign in issue

--- components/NoteCard.tsx
+++ components/NoteCard.tsx
@@ -11,26 +11,22 @@ interface Prop{
 const NoteCard = ({note}: Prop) => {
 
   return (
-    <article className="overflow-hidden rounded-lg shadow-sm transition hover:shadow-lg w-150 h-150">
-      <img
-        alt=""
-        src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-        className="h-56 w-full object-cover"
-      />
+    <article className="overflow-hidden rounded-lg shadow-sm transition hover:shadow-lg w-150 h-150 border-4">
+     
 
       <div className="bg-white p-4 sm:p-6">
         <time dateTime="2022-10-10" className="block text-xs text-gray-500">
           {" "}
-          {note.createdAt}{" "}
+          {note.createdAt.toLocaleString()}{" "}
         </time>
 
         <a href="#">
-          <h3 className="mt-0.5 text-lg text-gray-900">
+          <h3 className="mt-0.5 text-4xl text-gray-900 font-bold text-center">
             {note.title}
           </h3>
         </a>
 
-        <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+        <p className="mt-2 line-clamp-3 text-lg text-gray-500">
           {note.note}
         </p>
       </div>
